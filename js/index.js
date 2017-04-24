da2de7327e77554a95fec6351bd23fe43efe588c
sara-waleed-mostafa <sara.waleed.mostafa267@gmail.com>
2017-04-24 23:05:30 +0200

    materialize-sample

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function(){
         contentType: 'html', // or text
         // defaults to null for infinite loop
         loopCount: null,
-        callback: function(){ foo(); },
+        // callback: function(){ foo(); },
         resetCallback: function() { newTyped(); }
     });
 
