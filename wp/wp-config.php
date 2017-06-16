f1cc194542f09fd5a2b733565f08c475499516bf
marinaWagih <marina_wagih_cs@yahoo.com>
2017-06-16 21:59:40 +0200

    ignore config

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'egc');
+define('DB_NAME', 'wp-egc');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '0000000mrmr');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
